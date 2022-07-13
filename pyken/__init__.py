d1b9f35a52854bcd2aa2a43277936f8aa4764bfe
Guilliu <guille.lv.97@gmail.com>
2022-07-13 15:24:33 +0200

    New files

--- pyken/__init__.py
+++ pyken/__init__.py
@@ -1,28 +1,26 @@
-from pyken.memento import autoscorecard, auto_grouping, data_convert, adapt_data
-from pyken.carpe import (
-string_categories1, string_categories2, string_to_num, num_to_string,
-breakpoints_to_str, breakpoints_to_num, remapeo_missing, compute_iv,
-compute_group_names, compute_table, compute_scorecard,
-transform_to_woes, calib_score, apply_scorecard,
-transform_to_points, compute_final_breakpoints, compute_info, 
-features_selection, pretty_scorecard, parceling, cell_style, 
-mini_log, f_pd_read_hdfs_file, f_read_hdfs_model, check_if_model_exists,
-calcula_ks_pyspark, compute_pyspark_formula, proc_freq
-)
+from pyken.bojack import autoscorecard, auto_grouping
+from pyken.todd import (string_categories1, string_categories2, string_to_num, num_to_string, breakpoints_to_str,
+breakpoints_to_num, remapeo_missing, data_convert, adapt_data)
+from pyken.diane import (compute_iv, compute_group_names, compute_table,
+transform_to_woes, calib_score, compute_scorecard, transform_to_points, apply_scorecard)
+from pyken.mr_peanutbutter import pretty_scorecard, parceling, cell_style, proc_freq
+from pyken.princess_carolyn import compute_final_breakpoints, compute_info, features_selection, display_table_ng
+from pyken.utilities_pyspark import (f_pd_read_hdfs_file, f_read_hdfs_model,
+check_if_model_exists, compute_ks_pyspark, compute_pyspark_formula)
 
 
-__version__ = '0.1.78'
+__version__ = '0.2.4'
 
 
 __all__ = (
-    autoscorecard, auto_grouping, data_convert, adapt_data,
-    string_categories1, string_categories2, string_to_num, num_to_string,
-    breakpoints_to_str, breakpoints_to_num, remapeo_missing, compute_iv,
-    compute_group_names, compute_table, compute_scorecard,
-    transform_to_woes, calib_score, apply_scorecard,
-    transform_to_points, compute_final_breakpoints, compute_info, 
-    features_selection, pretty_scorecard, parceling, cell_style, 
-    mini_log, f_pd_read_hdfs_file, f_read_hdfs_model, check_if_model_exists,
-    calcula_ks_pyspark, compute_pyspark_formula, proc_freq
+    autoscorecard, auto_grouping,
+    string_categories1, string_categories2, string_to_num, num_to_string, breakpoints_to_str,
+    breakpoints_to_num, remapeo_missing, data_convert, adapt_data,
+    compute_iv, compute_group_names, compute_table,
+    transform_to_woes, calib_score, compute_scorecard, transform_to_points, apply_scorecard,
+    pretty_scorecard, parceling, cell_style, proc_freq,
+    compute_final_breakpoints, compute_info, features_selection, display_table_ng,
+    f_pd_read_hdfs_file, f_read_hdfs_model,
+    check_if_model_exists, compute_ks_pyspark, compute_pyspark_formula
 )
 
